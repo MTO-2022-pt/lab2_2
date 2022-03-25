9ebcd8706adf24e6cf0e31e25a34039384f8c287
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-03-25 18:43:19 +0100

    Testing results if either seq is empty

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
     public static final int[] emptySeq = {};
+    public static final int[] fourElementSeq = {1, 2, 3, 4};
+    public static final int[] anotherFourElementSeqTwoSimilar = {1, 3, 5, 6};
 
     /*
     ### Testy stanu ###
@@ -17,6 +19,18 @@ class SimilarityFinderTest {
     void bothSeqAreEmptyExpectingOne() {
         SimilarityFinder simFinder = new SimilarityFinder(((elem, sequence) -> SearchResult.builder().build()));
         assertEquals(1.0, simFinder.calculateJackardSimilarity(emptySeq, emptySeq));
+    }
+
+    @Test
+    void firstSeqIsEmptyExpectingZero() {
+        SimilarityFinder simFinder = new SimilarityFinder(((elem, sequence) -> SearchResult.builder().withFound(false).build()));
+        assertEquals(0.0, simFinder.calculateJackardSimilarity(emptySeq, fourElementSeq));
+    }
+
+    @Test
+    void secondSeqIsEmptyExpectingZero() {
+        SimilarityFinder simFinder = new SimilarityFinder(((elem, sequence) -> SearchResult.builder().withFound(false).build()));
+        assertEquals(0.0, simFinder.calculateJackardSimilarity(fourElementSeq, emptySeq));
     }
 
 }
