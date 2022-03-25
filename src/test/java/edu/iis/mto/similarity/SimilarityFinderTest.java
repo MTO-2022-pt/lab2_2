328d0e0ff78e08705cacf03cc877fb3b33da11eb
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-03-25 18:29:45 +0100

    Testing results if both seq are empty

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,22 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
+    public static final int[] emptySeq = {};
+
+    /*
+    ### Testy stanu ###
+     */
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void bothSeqAreEmptyExpectingOne() {
+        SimilarityFinder simFinder = new SimilarityFinder(((elem, sequence) -> SearchResult.builder().build()));
+        assertEquals(1.0, simFinder.calculateJackardSimilarity(emptySeq, emptySeq));
     }
 
 }
