63992fb0901757367a47d01efaa6ba226f129664
piotr-s-nowak <217728@edu.p.lodz.pl>
2022-03-30 23:32:42 +0200

    Added tests
    Added tests when sequences have two of the same elements and combined length of 10 and when they have three of the same elements and combined length of 9

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -64,10 +64,46 @@ class SimilarityFinderTest {
 
     @Test
     public void shouldReturnZeroWhenSecondSequenceIsEmpty() {
-        int[] seq1 = {3, 6};
+        int[] seq1 = {1, 2, 3};
         int[] seq2 = {};
 
         double result = finderFalse.calculateJackardSimilarity(seq1, seq2);
         assertEquals(0, result);
+    }
+
+    @Test
+    public void shouldReturnQuarterWhenSequencesHaveTwoSameElementsAndTotalLengthTen() {
+        int[] seq1 = {5, 6, 7, 8};
+        int[] seq2 = {1, 2, 3, 4, 5, 6};
+        SearchResult found = SearchResult.builder().withFound(true).build();
+        SearchResult notFound = SearchResult.builder().withFound(false).build();
+
+        SimilarityFinder finder = new SimilarityFinder((elem, sequence) -> {
+           if (elem == 5) return found;
+           else if (elem == 6) return found;
+           else if (elem == 7) return notFound;
+           else if (elem == 8) return notFound;
+           else return null;
+        });
+
+        double result = finderTrue.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(0.25d, result);
+    }
+
+    @Test
+    public void shouldReturnHalfWhenSequencesHaveThreeSameElementsAndTotalLengthNine() {
+        int[] seq1 = {3, 4, 5};
+        int[] seq2 = {1, 2, 3, 4, 5, 6};
+        SearchResult found = SearchResult.builder().withFound(true).build();
+
+        SimilarityFinder finder = new SimilarityFinder((elem, sequence) -> {
+            if (elem == 3) return found;
+            else if (elem == 4) return found;
+            else if (elem == 5) return found;
+            else return null;
+        });
+
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+        assertEquals(0.5d, result);
     }
 }
