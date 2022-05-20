6bf3fa0a0c7a443e1aefcd4bb73ccef9765f4024
karwowski.franciszek <228765@edu.p.lodz.pl>
2022-05-20 10:31:13 +0200

    dodalem test sprawdzajacy dwie puste sekwencje

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,20 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
-class SimilarityFinderTest {
+import edu.iis.mto.searcher.SearchResult;
 
+class SimilarityFinderTest {
+    public static final int[] empySequance = {};
+
+    // Testy stanu
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void checkIfReturnedOneIfBothSeqAreEmpty() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(((elem, sequance) -> SearchResult.builder().build()));
+        assertEquals(1.0, similarityFinder.calculateJackardSimilarity(empySequance, empySequance));
     }
 
 }
