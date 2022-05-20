793af6b3900359a4553840d30e17db4f365eda3d
karwowski.franciszek <228765@edu.p.lodz.pl>
2022-05-20 10:44:50 +0200

    dodalem testy sprawdzajace czy jest 0 jak jedna z sekwencji jest pusta

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,20 +1,30 @@
 package edu.iis.mto.similarity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
 import edu.iis.mto.searcher.SearchResult;
 
 class SimilarityFinderTest {
-    public static final int[] empySequance = {};
-
+    public static final int[] emptySequance = {};
+    public static final int[] fiveElementSequance = {1, 2, 3, 4, 5};
+    
+    SimilarityFinder similarityFinder = new SimilarityFinder(((elem, sequance) -> SearchResult.builder().build()));
     // Testy stanu
     @Test
     void checkIfReturnedOneIfBothSeqAreEmpty() {
-        SimilarityFinder similarityFinder = new SimilarityFinder(((elem, sequance) -> SearchResult.builder().build()));
-        assertEquals(1.0, similarityFinder.calculateJackardSimilarity(empySequance, empySequance));
+        assertEquals(1.0, similarityFinder.calculateJackardSimilarity(emptySequance, emptySequance));
+    }
+
+    @Test
+    void checkIfReturnedZeroIfFirstSeqIsEmpty() {
+        assertEquals(0.0, similarityFinder.calculateJackardSimilarity(emptySequance, fiveElementSequance));
+    }
+
+    @Test
+    void checkIfReturnedZeroIfSecondSeqIsEmpty() {
+        assertEquals(0.0, similarityFinder.calculateJackardSimilarity(fiveElementSequance, emptySequance));
     }
 
 }
