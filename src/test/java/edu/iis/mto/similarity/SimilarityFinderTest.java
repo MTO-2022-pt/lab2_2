c992d91360b92edeab04d9c5f19bbe1149e2e5c8
karwowski.franciszek <228765@edu.p.lodz.pl>
2022-05-20 10:58:05 +0200

    dodalem test do sprawdzania sekwencji o tej samej dlugosci

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -2,29 +2,43 @@ package edu.iis.mto.similarity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
 
 import edu.iis.mto.searcher.SearchResult;
+import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
     public static final int[] emptySequance = {};
     public static final int[] fiveElementSequance = {1, 2, 3, 4, 5};
+    public static final int[] secondFiveElementSequance = {1, 2, 7, 8, 9};
     
-    SimilarityFinder similarityFinder = new SimilarityFinder(((elem, sequance) -> SearchResult.builder().build()));
     // Testy stanu
     @Test
     void checkIfReturnedOneIfBothSeqAreEmpty() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(((elem, sequance) -> SearchResult.builder().build()));
         assertEquals(1.0, similarityFinder.calculateJackardSimilarity(emptySequance, emptySequance));
     }
 
     @Test
     void checkIfReturnedZeroIfFirstSeqIsEmpty() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(((elem, sequance) -> SearchResult.builder().build()));
         assertEquals(0.0, similarityFinder.calculateJackardSimilarity(emptySequance, fiveElementSequance));
     }
 
     @Test
     void checkIfReturnedZeroIfSecondSeqIsEmpty() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(((elem, sequance) -> SearchResult.builder().build()));
         assertEquals(0.0, similarityFinder.calculateJackardSimilarity(fiveElementSequance, emptySequance));
+    }
+
+    @Test
+    void sameLengthSequanceExpectingTwo() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(((elem, sequance) -> {
+            if (elem == 1 || elem == 2) return SearchResult.builder().withFound(true).build();
+            return SearchResult.builder().withFound(false).build();
+        }));
+        
+        
+        assertEquals(0.25, similarityFinder.calculateJackardSimilarity(fiveElementSequance, secondFiveElementSequance));
     }
 
 }
